bcdf4dc4911a6466741badff5ac19c146b5f8e8f
Richard Rondu <rondu.richard@lainwir3d.net>
2024-02-25 20:28:48 +0000

    Enable stock increase on credit bill when productbatch module is enabled

--- htdocs/core/class/conf.class.php
+++ htdocs/core/class/conf.class.php
@@ -616,7 +616,7 @@ class Conf
 			if (!empty($this->productbatch->enabled)) {
 				$this->global->STOCK_CALCULATE_ON_BILL = 0;
 				$this->global->STOCK_CALCULATE_ON_INVOICE_BILL = 0;
-				$this->global->STOCK_CALCULATE_ON_CREDIT_BILL = 0;
+				$this->global->STOCK_CALCULATE_ON_CREDIT_BILL = 1;
 				$this->global->STOCK_CALCULATE_ON_VALIDATE_ORDER = 0;
 				if (empty($this->global->STOCK_CALCULATE_ON_SHIPMENT_CLOSE)) $this->global->STOCK_CALCULATE_ON_SHIPMENT = 1;
 				if (empty($this->global->STOCK_CALCULATE_ON_SHIPMENT)) $this->global->STOCK_CALCULATE_ON_SHIPMENT_CLOSE = 1;
